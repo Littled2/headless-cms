520a99457e4a7fa1d4a58fbb803eaec4f5ab952c
Ed <edward.blewitt2@gmail.com>
2023-09-07 17:05:05 +0100

    Fixed issue with CSR if initial page did not have a title

--- headless-cms-scripts/client-side-router.js
+++ headless-cms-scripts/client-side-router.js
@@ -24,7 +24,7 @@ class Client_Side_Router {
         this.cache = {
             [window.location.href]: new Cache_Item({
                 settings: {
-                    title: document.title,
+                    title: document?.title,
                     description: description ? description : '',
                     favicon: favicon ? favicon : ''
                 },
